533510a0eabc584be27d9bca19cdf2a5968e8f08
n.merkulova <n.merkulova@ktsstudio.ru>
2023-01-27 19:23:27 +0400

    [*] issue-1

--- src/hooks/useEventSubscribe.ts
+++ src/hooks/useEventSubscribe.ts
@@ -8,18 +8,18 @@ import { useCallback, useEffect, DependencyList } from 'react';
  * Хук для подписки на событие vk-bridge.
  *
  * @param {AnyReceiveMethodName} eventName Название события.
- * @param {VoidFunction} callBack Коллбэк, вызываемый при наступлении события.
+ * @param {(event: VKBridgeEvent<AnyReceiveMethodName>) => void} callBack Коллбэк, вызываемый при наступлении события.
  * @param {React.DependencyList} [deps=[]] Зависимости переданного коллбэка. По умолчанию пустой массив.
  */
 const useEventSubscribe = (
   eventName: AnyReceiveMethodName,
-  callBack: VoidFunction,
+  callBack: (event: VKBridgeEvent<AnyReceiveMethodName>) => void,
   deps: DependencyList = []
 ): void => {
   const eventListener = useCallback(
     (event: VKBridgeEvent<AnyReceiveMethodName>) => {
       if (event.detail.type === eventName) {
-        callBack();
+        callBack(event);
       }
     },
     deps
